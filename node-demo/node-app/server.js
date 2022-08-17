79dc563157afcd25fad2d1321ff83ce226950f15
康仔 <1336738527@qq.com>
2022-08-17 16:47:37 +0800

    20220817

--- node-demo/node-app/server.js
+++ node-demo/node-app/server.js
@@ -3,10 +3,19 @@ const express = require("express");
 
 // 引入mongoose
 const mongoose = require("mongoose");
+
+const bodyParser = require("body-parser");
 // 实例化
 const app = express();
+//引入users.js
+const users = require("./routes/api/users");
+
 // DB config
 const db = require("./config/keys").mongoURL;
+
+// 使用body-parser中间件
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 // connect to mongodb
 // const dbURL = "mongodb://127.0.0.1:27017/nodeapi";
 mongoose.connect(db)
@@ -17,7 +26,11 @@ mongoose.connect(db)
 // 设置路由
 app.get("/", (req, res) => {
     res.send("Hello World!");
-})
+});
+
+
+// 使用router
+app.use("/api/users", users);
 // 设置端口号
 const port = process.env.PORT || 5000;
 // 监听
